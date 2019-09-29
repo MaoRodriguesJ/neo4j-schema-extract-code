e4c55598f6a71d85b52455436420a016684e4054
MaoRodriguesJ <maorodriguesj@gmail.com>
2019-09-29 17:44:05 -0300

    grouping finished

--- query.py
+++ query.py
@@ -4,13 +4,16 @@ class Query():
     def _get_types(tx):
         return tx.run("call db.relationshipTypes")
 
+
     @staticmethod
     def _get_labels(tx):
         return tx.run("call db.labels")
 
+
     @staticmethod
     def _get_schema(tx):
         return tx.run("call db.schema")
+
 
     @staticmethod
     def _get_nodes_by_label(tx, params):
@@ -21,7 +24,14 @@ class Query():
         query = query + "length(labels(node))=" + str(len(params)) + " return node"
         return tx.run(query, params)
 
+
     @staticmethod
-    def _get_relations_types_by_id(tx, params):
-        return tx.run("match (node)-[relation]->(end_node) where id(node)=$id "+
-                      "return type(relation) as relation, labels(end_node) as labels", params)
+    def _get_relationships_types_by_id(tx, params):
+        return tx.run("match (node)-[relationship]->(end_node) where id(node)=$id "+
+                      "return type(relationship) as relationship, labels(end_node) as labels", params)
+
+
+    @staticmethod
+    def _get_relationships_by_type(tx, params):
+        return tx.run("match ()-[relationship]-() where type(relationship)=$type return relationship", params)
+    
--- schema.py
+++ schema.py
@@ -31,18 +31,41 @@ class SchemaExtractor():
         return self._driver.read_transaction(Query._get_nodes_by_label, params)
 
     
-    def get_relations_types_by_id(self, node_id):
+    def get_relationships_types_by_id(self, node_id):
         params = {'id' : node_id}
-        return self._driver.read_transaction(Query._get_relations_types_by_id, params)
+        return self._driver.read_transaction(Query._get_relationships_types_by_id, params)
 
 
     def get_relationship_types(self):
         return [t['relationshipType'] for t in self._driver.read_transaction(Query._get_types)]
 
+    
+    def get_relationships_by_type(self, typed):
+        params = {'type': typed}
+        return self._driver.read_transaction(Query._get_relationships_by_type, params)
 
-    def process_properties_grouping(self, grouping, key, record):
-        for prop in record['node'].keys():
-            typed = type(record['node'].get(prop))
+
+    def post_process(self, grouping):
+        for key in grouping:
+            size = key[1]
+            for prop in grouping[key]:
+                if grouping[key][prop] == size:
+                    grouping[key][prop] = True
+                elif prop != 'relationships':
+                    grouping[key][prop] = False
+
+    
+    def process_keys(self, grouping, new_key_name):
+        processed_grouping = dict()
+        for key in grouping:
+            processed_grouping[(key[0], new_key_name)] = grouping[key]
+
+        return processed_grouping
+
+
+    def process_props_grouping(self, grouping, key, record, record_name):
+        for prop in record[record_name].keys():
+            typed = type(record[record_name].get(prop))
             prop_key = (prop, typed)
             if prop_key in grouping[key].keys():
                 grouping[key][prop_key] = grouping[key][prop_key] + 1
@@ -50,46 +73,68 @@ class SchemaExtractor():
                 grouping[key][prop_key] = 1
 
 
-    def process_relations_grouping(self, grouping, key, node_id):
-        relationships = self.get_relations_types_by_id(node_id)
+    def process_relationships_grouping(self, grouping, key, node_id):
+        relationships = self.get_relationships_types_by_id(node_id)
         if 'relationships' not in grouping[key].keys():
             grouping[key]['relationships'] = dict()
             for r in relationships:
-                relation_key = (r['relation'], frozenset(r['labels']))
-                grouping[key]['relationships'][relation_key] = True
+                relationship_key = (r['relationship'], r['labels'][0])
+                grouping[key]['relationships'][relationship_key] = True
         
         else:
             relationships_keys = set()
             old_keys = grouping[key]['relationships'].keys()
             for r in relationships:
-                relation_key = (r['relation'], frozenset(r['labels']))
-                relationships_keys.add(relation_key)
-                if relation_key not in old_keys:
-                    grouping[key]['relationships'][relation_key] = False
+                relationship_key = (r['relationship'], r['labels'][0])
+                relationships_keys.add(relationship_key)
+                if relationship_key not in old_keys:
+                    grouping[key]['relationships'][relationship_key] = False
 
             for k in old_keys:
                 if k not in relationships_keys:
                     grouping[key]['relationships'][k] = False
 
 
-    def process_relations_props(self, grouping):
-        return None
-
-
-    def grouping(self):
+    def grouping_nodes(self):
         labels_powerset = self.get_labels_combination(self.get_labels())
         grouping = dict()
         for label_combination in labels_powerset:
             records = [r for r in self.get_nodes_by_labels(label_combination)]
+            if len(records) == 0:
+                continue
+
             key = (label_combination, len(records))
             grouping[key] = dict()
             for record in records:
-                self.process_properties_grouping(grouping, key, record)
-                self.process_relations_grouping(grouping, key, record['node'].id)
+                self.process_props_grouping(grouping, key, record, 'node')
+                self.process_relationships_grouping(grouping, key, record['node'].id)
 
-        self.process_relations_props(grouping)
-        
-        return grouping
+        self.post_process(grouping)
+        return self.process_keys(grouping, 'node')
+
+
+    def grouping_relationships(self):
+        types = self.get_relationship_types()
+        grouping = dict()
+        for typed in types:
+            records = [r for r in self.get_relationships_by_type(typed)]
+            if len(records) == 0:
+                continue
+
+            key = (typed, len(records))
+            grouping[key] = dict()
+            for record in records:
+                self.process_props_grouping(grouping, key, record, 'relationship')
+
+        self.post_process(grouping)
+        return self.process_keys(grouping, 'relationship')
+
+
+    def grouping(self):
+        grouping_nodes = self.grouping_nodes()
+        grouping_relationships = self.grouping_relationships()
+ 
+        return {**grouping_nodes, **grouping_relationships}
 
 
 if __name__ == '__main__':
